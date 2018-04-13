00f1cb0b0996056a2ae941c0bf8d7eb1d1aae146
jacy <jacy@emucoo.com>
2018-04-13 14:07:46 +0800

    初始创建

--- emucoo-service/src/main/java/com/emucoo/mapper/SysUserMapper.java
+++ emucoo-service/src/main/java/com/emucoo/mapper/SysUserMapper.java
@@ -3,13 +3,5 @@ package com.emucoo.mapper;
 import com.emucoo.common.base.mapper.MyMapper;
 import com.emucoo.model.SysUser;
 
-import java.util.List;
-
 public interface SysUserMapper extends MyMapper<SysUser> {
-
-    /**
-     * 查询用户的所有权限
-     * @param userId  用户ID
-     */
-    List<String> queryAllPerms(Long userId);
 }
--- emucoo-service/src/main/java/com/emucoo/mapper/TImgAppendMapper.java
+++
@@ -1,7 +0,0 @@
-package com.emucoo.mapper;
-
-import com.emucoo.common.base.mapper.MyMapper;
-import com.emucoo.model.TImgAppend;
-
-public interface TImgAppendMapper extends MyMapper<TImgAppend> {
-}
