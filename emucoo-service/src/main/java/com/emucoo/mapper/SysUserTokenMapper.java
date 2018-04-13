9c74ac5360aafcd231b425748f9a98893082370e
jacy <jacy@emucoo.com>
2018-04-13 14:12:50 +0800

    初始创建

---
+++ emucoo-service/src/main/java/com/emucoo/mapper/SysUserTokenMapper.java
@@ -0,0 +1,7 @@
+package com.emucoo.mapper;
+
+import com.emucoo.common.base.mapper.MyMapper;
+import com.emucoo.model.SysUserToken;
+
+public interface SysUserTokenMapper extends MyMapper<SysUserToken> {
+}
