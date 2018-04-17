b509bb79f59ec2b76247c1c882c1103765c34400
jacy <jacy@emucoo.com>
2018-04-17 14:20:31 +0800

    合并代码

--- emucoo-manager/src/main/java/com/emucoo/manager/controller/sys/SysPostController.java
+++ emucoo-manager/src/main/java/com/emucoo/manager/controller/sys/SysPostController.java
@@ -1,5 +1,7 @@
 package com.emucoo.manager.controller.sys;
 
+import com.emucoo.common.base.rest.ApiResult;
+import com.emucoo.common.base.rest.BaseResource;
 import com.emucoo.common.util.R;
 import com.emucoo.dto.base.ParamVo;
 import com.emucoo.manager.controller.AbstractController;
@@ -20,7 +22,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/sys/post")
-public class SysPostController extends AbstractController {
+public class SysPostController extends BaseResource {
 	@Autowired
 	private SysPostService sysPostService;
 	
@@ -29,14 +31,16 @@ public class SysPostController extends AbstractController {
 	 */
 	@PostMapping ("/list")
 	@RequiresPermissions("sys:post:list")
-	public R list(@RequestBody ParamVo<SysPost> param){
+	@ResponseBody
+	public ApiResult list(@RequestBody ParamVo<SysPost> param){
 		SysPost sysPost = param.getData();
 		Example example=new Example(SysPost.class);
 		example.createCriteria().andLike("postName",sysPost.getPostName());
 		PageHelper.startPage(param.getPageNumber(), param.getPageSize(), "create time desc");
 		List<SysPost> postList = sysPostService.selectByExample(example);
 		PageInfo<SysPost> pageInfo=new PageInfo(postList);
-	    return  R.ok().put("data",pageInfo);
+		return success(pageInfo);
+
 	}
 
 
@@ -45,30 +49,33 @@ public class SysPostController extends AbstractController {
 	 */
 	@PostMapping ("/save")
 	@RequiresPermissions("sys:post:save")
-	public R save(@RequestBody SysPost post){
+	@ResponseBody
+	public ApiResult save(@RequestBody SysPost post){
 		sysPostService.save(post);
 
-		return R.ok();
+		return success("success");
 	}
 
 	/**
 	 * 修改岗位
 	 */
-	@PutMapping ("/update")
+	@PostMapping ("/update")
 	@RequiresPermissions("sys:post:update")
-	public R update(@RequestBody SysPost post){
+	@ResponseBody
+	public ApiResult update(@RequestBody SysPost post){
 		sysPostService.update(post);
-		return R.ok();
+		return success("success");
 	}
 
 	/**
 	 * 删除岗位
 	 */
-	@DeleteMapping ("/delete")
+	@PostMapping ("/delete")
 	@RequiresPermissions("sys:post:delete")
-	public R delete(Long id){
+	@ResponseBody
+	public ApiResult delete(Long id){
 		sysPostService.deleteById(id);
 
-		return R.ok();
+		return success("success");
 	}
 }
