dfb1c33e0b5582df053b86e9032ea77cc37b3d88
Shayne <shayne@emucoo.com>
2018-04-18 15:10:18 +0800

    add default value for response of chance points

--- emucoo-manager/src/main/java/com/emucoo/manager/controller/ChancePointController.java
+++ emucoo-manager/src/main/java/com/emucoo/manager/controller/ChancePointController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -29,6 +30,8 @@ public class ChancePointController extends BaseResource {
         int pageSz = param.getPageSize();
         List<TOpportunity> opptList = chancePointService.listChancePointsByNameKeyword(keyword, pageNm, pageSz);
 
+        if(opptList == null || opptList.size() == 0)
+            opptList = new ArrayList<>();
         return success(opptList);
     }
 
