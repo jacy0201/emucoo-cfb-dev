4fb2442f441675aca4c40c2a5f4ce1229166edca
jacy <jacy@emucoo.com>
2018-04-23 09:40:29 +0800

    token 类修改

--- emucoo-service/src/main/java/com/emucoo/service/sys/imp/SysUserTokenServiceImpl.java
+++ emucoo-service/src/main/java/com/emucoo/service/sys/imp/SysUserTokenServiceImpl.java
@@ -1,17 +1,15 @@
 package com.emucoo.service.sys.imp;
 
 import com.emucoo.common.base.service.impl.BaseServiceImpl;
-import com.emucoo.common.util.R;
-import com.emucoo.common.util.TokenGenerator;
+import com.emucoo.common.security.token.TokenUtils;
 import com.emucoo.mapper.SysUserTokenMapper;
 import com.emucoo.model.SysUserToken;
 import com.emucoo.service.sys.SysUserTokenService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  *
@@ -22,8 +20,9 @@ import java.util.Map;
 public class SysUserTokenServiceImpl extends BaseServiceImpl<SysUserToken> implements SysUserTokenService {
 	@Autowired
 	private SysUserTokenMapper sysUserTokenMapper;
-	//12小时后过期
-	private final static int EXPIRE = 3600 * 12;
+	// token 过期时间， 默认2小时后过期
+	@Value("${token.expire}")
+	private int expire;
 	
 	@Override
 	public SysUserToken queryByToken(String token) {
@@ -33,11 +32,11 @@ public class SysUserTokenServiceImpl extends BaseServiceImpl<SysUserToken> imple
 	@Override
 	public SysUserToken getToken(long userId) {
 		//生成一个token
-		String token = TokenGenerator.generateValue();
+		String token = TokenUtils.generateValue();
 		//当前时间
 		Date now = new Date();
 		//过期时间
-		Date expireTime = new Date(now.getTime() + EXPIRE * 1000);
+		Date expireTime = new Date(now.getTime() + expire * 1000 * 3600);
 
 		//判断是否生成过token
 		SysUserToken tokenEntity = sysUserTokenMapper.selectByPrimaryKey(userId);
