935753b36a3834f2e4346fa5a1897d893a74dc0c
liuhp <Emucoo2018>
2018-04-26 11:15:35 +0800

    增加表单结果类

---
+++ emucoo-service/src/main/java/com/emucoo/mapper/TFormCheckResultMapper.java
@@ -0,0 +1,7 @@
+package com.emucoo.mapper;
+
+import com.emucoo.common.base.mapper.MyMapper;
+import com.emucoo.model.TFormCheckResult;
+
+public interface TFormCheckResultMapper extends MyMapper<TFormCheckResult> {
+}
