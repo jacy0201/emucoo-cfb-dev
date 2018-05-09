993ff3f1b3605bfca49edc5747dbbcf35bb75805
liuhp <Emucoo2018>
2018-05-09 14:05:38 +0800

    修改计划分页

--- emucoo-service/src/main/java/com/emucoo/service/manage/impl/LoopPlanManageServiceImpl.java
+++ emucoo-service/src/main/java/com/emucoo/service/manage/impl/LoopPlanManageServiceImpl.java
@@ -99,8 +99,8 @@ public class LoopPlanManageServiceImpl extends BaseServiceImpl<TLoopPlan> implem
         tPlanFormRelationMapper.deleteById(plan.getId());
     }
 
-    public List<TLoopPlan> findPlanListByCondition(TLoopPlan plan, Integer pageNumber, Integer pageSize) {
-        List<TLoopPlan> planList = tLoopPlanMapper.findPlanListByCondition(plan, pageSize, pageSize * (pageNumber - 1));
+    public List<TLoopPlan> findPlanListByCondition(TLoopPlan plan) {
+        List<TLoopPlan> planList = tLoopPlanMapper.findPlanListByCondition(plan);
         return planList;
     }
 
