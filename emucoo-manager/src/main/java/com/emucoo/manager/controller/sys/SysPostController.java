6c1bbf591ac1be168a859d9851dc9a5c4ed07ed8
jacy <jacy@emucoo.com>
2018-05-09 11:37:34 +0800

    修改字段

--- emucoo-manager/src/main/java/com/emucoo/manager/controller/sys/SysPostController.java
+++ emucoo-manager/src/main/java/com/emucoo/manager/controller/sys/SysPostController.java
@@ -109,7 +109,8 @@ public class SysPostController extends BaseResource {
 	@ApiOperation(value="删除岗位")
 	public ApiResult delete(@RequestBody SysPost post){
 		if(post.getId()==null){return fail(ApiExecStatus.INVALID_PARAM,"id 参数不能为空!");}
-		sysPostService.deleteById(post.getId());
+		post.setIsDel(true);
+		sysPostService.updateSelective(post);
 		return success("success");
 	}
 }
