e0a11f8f344414b958aa9c74a5f8b41f6c4f5310
jacy <jacy@emucoo.com>
2018-05-15 18:29:29 +0800

    行事历

--- emucoo-restApi/src/main/java/com/emucoo/restApi/controller/calendar/CalendarController.java
+++ emucoo-restApi/src/main/java/com/emucoo/restApi/controller/calendar/CalendarController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 import java.util.List;
 
@@ -36,9 +35,6 @@ public class CalendarController extends AppBaseController {
 
     @Resource
     private CalendarService calendarService;
-
-    @Resource
-    private SysUserService sysUserService;
 
     /**
      * 获取指定用户,指定月份行事历
