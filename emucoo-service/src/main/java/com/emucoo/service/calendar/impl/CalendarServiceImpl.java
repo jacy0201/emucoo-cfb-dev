d1f5b2b51e43eab71e3447ed44ee6f64f1ac0c69
jacy <jacy@emucoo.com>
2018-05-18 12:17:05 +0800

    行事历

--- emucoo-service/src/main/java/com/emucoo/service/calendar/impl/CalendarServiceImpl.java
+++ emucoo-service/src/main/java/com/emucoo/service/calendar/impl/CalendarServiceImpl.java
@@ -1,5 +1,6 @@
 package com.emucoo.service.calendar.impl;
 
+import com.emucoo.dto.base.ISystem;
 import com.emucoo.dto.modules.calendar.CalendarListDateIn;
 import com.emucoo.dto.modules.calendar.CalendarListDateOut;
 import com.emucoo.dto.modules.calendar.CalendarListMonthIn;
@@ -9,18 +10,20 @@ import com.emucoo.mapper.*;
 import com.emucoo.model.*;
 import com.emucoo.service.calendar.CalendarService;
 import com.emucoo.utils.ConstantsUtil;
+import org.apache.commons.lang.ArrayUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import redis.clients.jedis.JedisCluster;
 import tk.mybatis.mapper.entity.Example;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.List;
+import java.text.Collator;
+import java.util.*;
 
 /**
  * 行事历
- * @author Jacy
  *
+ * @author Jacy
  */
 @Transactional
 @Service
@@ -40,64 +43,89 @@ public class CalendarServiceImpl implements CalendarService {
     private TTaskMapper taskMapper;
     @Resource
     private SysUserMapper sysUserMapper;
+    @Resource
+    private JedisCluster jedisCluster;
 
-    public CalendarListMonthOut listCalendarMonth(CalendarListMonthIn calendarListIn){
-        CalendarListMonthOut calendarListMonthOut =new CalendarListMonthOut();
+    public CalendarListMonthOut listCalendarMonth(CalendarListMonthIn calendarListIn, Long currentUserId) {
+        CalendarListMonthOut calendarListMonthOut = new CalendarListMonthOut();
         WorkVo_O.Work work = null;
-        List<WorkVo_O.Work> workArr=new ArrayList<>();
-        calendarListMonthOut.setMonth( calendarListIn.getMonth());
+        List<WorkVo_O.Work> workArr = new ArrayList<>();
+        calendarListMonthOut.setMonth(calendarListIn.getMonth());
         calendarListMonthOut.setUserId(calendarListIn.getUserId());
-        String yearStr = calendarListIn.getMonth().substring(0,4);
-        String monthStr = calendarListIn.getMonth().substring(4,6);
-        Example example=new Example(TFrontPlan.class);
-        example.createCriteria().andEqualTo("arrangeeId",calendarListIn.getUserId())
-        .andEqualTo("arrangeYear",yearStr).andEqualTo("arrangeMonth",monthStr)
-        .andEqualTo("isDel",false);
+        String yearStr = calendarListIn.getMonth().substring(0, 4);
+        String monthStr = calendarListIn.getMonth().substring(4, 6);
+        Example example = new Example(TFrontPlan.class);
+        example.createCriteria().andEqualTo("arrangeeId", calendarListIn.getUserId())
+                .andEqualTo("arrangeYear", yearStr).andEqualTo("arrangeMonth", monthStr)
+                .andEqualTo("isDel", false);
         example.setOrderByClause("plan_precise_time desc");
-        List<TFrontPlan> list= tFrontPlanMapper.selectByExample(example);
+        List<TFrontPlan> list = tFrontPlanMapper.selectByExample(example);
         //设置巡店安排
-        if(null!=list && list.size()>0){
-            for (TFrontPlan frontPlan:list){
+        if (null != list && list.size() > 0) {
+            for (TFrontPlan frontPlan : list) {
                 work = getFrontPlanWork(frontPlan);
                 workArr.add(work);
             }
             //设置 常规任务,指派任务，改善任务
-            List<TLoopWork> loopWorkList= tLoopWorkMapper.calendarMonthList(calendarListIn.getUserId(),yearStr,monthStr);
-            if(null!=loopWorkList && loopWorkList.size()>0){
-                for (TLoopWork tLoopWork :loopWorkList){
+            List<TLoopWork> loopWorkList = tLoopWorkMapper.calendarMonthList(calendarListIn.getUserId(), yearStr, monthStr);
+            if (null != loopWorkList && loopWorkList.size() > 0) {
+                for (TLoopWork tLoopWork : loopWorkList) {
                     work = getLoopWork(tLoopWork);
                     workArr.add(work);
                 }
             }
         }
         calendarListMonthOut.setWorkArr(workArr);
+        //设置最近联系人顺序
+        setUserOrder(calendarListIn.getUserId(), currentUserId);
         return calendarListMonthOut;
     }
 
+    /**
+     * 设置最近联系人顺序
+     *
+     * @param queryUserId   被查询人id
+     * @param currentUserId 当前登录用户id
+     */
+    private void setUserOrder(Long queryUserId, Long currentUserId) {
+        String userStr = jedisCluster.get(ISystem.IUSER.USER_RECENT + currentUserId);
+        String[] userIdArr = userStr.split(",");
+        if (ArrayUtils.isNotEmpty(userIdArr)) {
+            //元素后移
+            for (int i = userIdArr.length - 1; i > 0; i--) {
+                userIdArr[i] = userIdArr[i - 1];
+            }
+            //先删除被查看人员
+            userIdArr = (String[]) ArrayUtils.removeElement(userIdArr, queryUserId.toString());
+        }
+        //将被查看人员重新设置到第一位
+        userIdArr[0] = queryUserId.toString();
+        jedisCluster.set(ISystem.IUSER.USER_RECENT + currentUserId, ArrayUtils.toString(userIdArr));
+    }
 
 
-    public CalendarListDateOut listCalendarDate(CalendarListDateIn calendarListIn){
-        CalendarListDateOut calendarListDateOut =new CalendarListDateOut();
+    public CalendarListDateOut listCalendarDate(CalendarListDateIn calendarListIn) {
+        CalendarListDateOut calendarListDateOut = new CalendarListDateOut();
         WorkVo_O.Work work = null;
-        List<WorkVo_O.Work> workArr=new ArrayList<>();
-        calendarListDateOut.setDate( calendarListIn.getExecuteDate());
+        List<WorkVo_O.Work> workArr = new ArrayList<>();
+        calendarListDateOut.setDate(calendarListIn.getExecuteDate());
         calendarListDateOut.setUserId(calendarListIn.getUserId());
-        Example example=new Example(TFrontPlan.class);
-        example.createCriteria().andEqualTo("arrangeeId",calendarListIn.getUserId())
-                .andEqualTo("plan_date",calendarListIn.getExecuteDate())
-                .andEqualTo("isDel",false);
+        Example example = new Example(TFrontPlan.class);
+        example.createCriteria().andEqualTo("arrangeeId", calendarListIn.getUserId())
+                .andEqualTo("plan_date", calendarListIn.getExecuteDate())
+                .andEqualTo("isDel", false);
         example.setOrderByClause("plan_precise_time desc");
-        List<TFrontPlan> list= tFrontPlanMapper.selectByExample(example);
+        List<TFrontPlan> list = tFrontPlanMapper.selectByExample(example);
         //设置巡店安排
-        if(null!=list && list.size()>0){
-            for (TFrontPlan frontPlan:list){
+        if (null != list && list.size() > 0) {
+            for (TFrontPlan frontPlan : list) {
                 work = getFrontPlanWork(frontPlan);
                 workArr.add(work);
             }
             //设置 常规任务,指派任务，改善任务
-            List<TLoopWork> loopWorkList= tLoopWorkMapper.calendarDateList(calendarListIn.getUserId(),calendarListIn.getExecuteDate() );
-            if(null!=loopWorkList && loopWorkList.size()>0){
-                for (TLoopWork tLoopWork :loopWorkList){
+            List<TLoopWork> loopWorkList = tLoopWorkMapper.calendarDateList(calendarListIn.getUserId(), calendarListIn.getExecuteDate());
+            if (null != loopWorkList && loopWorkList.size() > 0) {
+                for (TLoopWork tLoopWork : loopWorkList) {
                     work = getLoopWork(tLoopWork);
                     workArr.add(work);
                 }
@@ -109,29 +137,29 @@ public class CalendarServiceImpl implements CalendarService {
     }
 
     private WorkVo_O.Work getFrontPlanWork(TFrontPlan frontPlan) {
-        WorkVo_O.Work work=new WorkVo_O.Work();
+        WorkVo_O.Work work = new WorkVo_O.Work();
         work.setId(frontPlan.getId());
         work.setSubID(frontPlan.getSubPlanId().toString());
         work.setWorkID(frontPlan.getLoopPlanId().toString());
         work.setWorkType(ConstantsUtil.LoopWork.TYPE_FOUR);
-        WorkVo_O.Work.Inspection inspection=work.getInspection();
+        WorkVo_O.Work.Inspection inspection = work.getInspection();
         inspection.setInspStartTime(frontPlan.getPlanPreciseTime());
         inspection.setInspStatus(frontPlan.getStatus().intValue());
         //查询表单
-        Example exampleForm=new Example(TFrontPlanForm.class);
-        exampleForm.createCriteria().andEqualTo("frontPlanId",frontPlan.getId()).andEqualTo("isDel",false);
-        List<TFrontPlanForm> formList=tFrontPlanFormMapper.selectByExample(exampleForm);
-        if(null!=formList && formList.size()>0){
-            TShopInfo shopInfo=tShopInfoMapper.selectByPrimaryKey(frontPlan.getShopId());
-            TFormMain tFormMain= tFormMainMapper.selectByPrimaryKey(formList.get(0).getFormMainId());
-            inspection.setInspTitle(shopInfo.getShopName()+tFormMain.getName()+"检查");
+        Example exampleForm = new Example(TFrontPlanForm.class);
+        exampleForm.createCriteria().andEqualTo("frontPlanId", frontPlan.getId()).andEqualTo("isDel", false);
+        List<TFrontPlanForm> formList = tFrontPlanFormMapper.selectByExample(exampleForm);
+        if (null != formList && formList.size() > 0) {
+            TShopInfo shopInfo = tShopInfoMapper.selectByPrimaryKey(frontPlan.getShopId());
+            TFormMain tFormMain = tFormMainMapper.selectByPrimaryKey(formList.get(0).getFormMainId());
+            inspection.setInspTitle(shopInfo.getShopName() + tFormMain.getName() + "检查");
         }
         work.setInspection(inspection);
         return work;
     }
 
     private WorkVo_O.Work getLoopWork(TLoopWork tLoopWork) {
-        WorkVo_O.Work work=new WorkVo_O.Work();
+        WorkVo_O.Work work = new WorkVo_O.Work();
         work.setId(tLoopWork.getId());
         work.setWorkID(tLoopWork.getWorkId());
         work.setWorkType(tLoopWork.getType());
@@ -149,17 +177,58 @@ public class CalendarServiceImpl implements CalendarService {
         return work;
     }
 
-    public List<SysUser> listLowerUser(Long userId){
-        List<SysUser> list=new ArrayList<>();
-        String userIds=sysUserMapper.findAllChildListByParentId(userId);
-        String [] userArr=userIds.split(",");
-       if(userArr.length>2){
-          for (int i=2;i<userArr.length;i++){
-              SysUser sysUser=sysUserMapper.selectByPrimaryKey(Long.parseLong(userArr[i]));
-              list.add(sysUser);
-          }
-       }
-        return list;
+    /**
+     * 查询当前用户最近查看用户列表
+     * @param currentUserId
+     * @return
+     */
+    public List<SysUser> listLowerUser(Long currentUserId) {
+        String userIds = sysUserMapper.findAllChildListByParentId(currentUserId);
+        String[] userArr = userIds.split(",");
+        List<SysUser> list = null;
+        if (null != userArr && userArr.length > 2) {
+            list = new ArrayList<>();
+            ArrayUtils.remove(userArr, 0);
+            ArrayUtils.remove(userArr, 1);
+            for (int i = 0; i < userArr.length; i++) {
+                SysUser sysUser = sysUserMapper.selectByPrimaryKey(Long.parseLong(userArr[i]));
+                list.add(sysUser);
+            }
+        }
+        //返回按姓名首字母排序的集合
+        if (null != list && list.size() > 0)
+            return orderUserList(list, currentUserId);
+        else
+            return null;
     }
 
+    //根据姓名首字母排序
+    private List<SysUser> orderUserList(List<SysUser> list, Long currentUserId) {
+        HashMap<String, SysUser> map = new HashMap<>();
+        String[] nameArr = new String[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            nameArr[i] = list.get(i).getRealName();
+            map.put(nameArr[i], list.get(i));
+        }
+        Comparator<Object> comparator = Collator.getInstance(java.util.Locale.CHINA);
+        Arrays.sort(nameArr, comparator);
+        list.clear();
+        String [] userArr=new String[list.size()];
+        if (!jedisCluster.exists(ISystem.IUSER.USER_RECENT + currentUserId)) {
+            for (int n = nameArr.length - 1; n >= 0; n--) {
+                if (map.containsKey(nameArr[n])) {
+                    userArr[n] = map.get(nameArr[n]).getId().toString();
+                }
+            }
+            jedisCluster.set(ISystem.IUSER.USER_RECENT + currentUserId, ArrayUtils.toString(userArr));
+        } else {
+            String userStr = jedisCluster.get(ISystem.IUSER.USER_RECENT + currentUserId);
+            String[] userIdArr = userStr.split(",");
+            for (int n = 0; n < userIdArr.length; n++) {
+                SysUser sysUser = sysUserMapper.selectByPrimaryKey(Long.parseLong(userIdArr[n]));
+                list.add(sysUser);
+            }
+        }
+        return list;
+    }
 }
