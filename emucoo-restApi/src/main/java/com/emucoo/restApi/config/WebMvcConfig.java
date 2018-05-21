1bc0bb6189d3a96d2a89822c12fed4ee0d6a411c
jacy <jacy@emucoo.com>
2018-05-21 16:13:45 +0800

    修改配置

--- emucoo-restApi/src/main/java/com/emucoo/restApi/config/WebMvcConfig.java
+++ emucoo-restApi/src/main/java/com/emucoo/restApi/config/WebMvcConfig.java
@@ -1,13 +1,23 @@
 package com.emucoo.restApi.config;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.DateSerializer;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.emucoo.restApi.config.interceptor.ApiInterceptor;
 import com.emucoo.restApi.config.interceptor.FormTokenInterceptor;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author fujg
@@ -38,5 +48,23 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
         registry.addInterceptor(new FormTokenInterceptor()).addPathPatterns("/**");
         registry.addInterceptor(new ApiInterceptor()).addPathPatterns("/api/**").excludePathPatterns("/api/index/login").excludePathPatterns("/api/index/resetPwd");
         super.addInterceptors(registry);
+
+    }
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        MappingJackson2HttpMessageConverter jackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper objectMapper = jackson2HttpMessageConverter.getObjectMapper();
+
+        //生成json时，将所有Long转换成String
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+        simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        simpleModule.addSerializer(Date.class, new DateSerializer(false, new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")));
+
+        objectMapper.registerModule(simpleModule);
+
+        jackson2HttpMessageConverter.setObjectMapper(objectMapper);
+        converters.add(0, jackson2HttpMessageConverter);
     }
 }
