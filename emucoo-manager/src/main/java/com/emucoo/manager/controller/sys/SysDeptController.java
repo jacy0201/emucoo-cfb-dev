67972ba99f6b0302daa63001301e1c9fbc7e4e11
jacy <jacy@emucoo.com>
2018-05-24 12:08:30 +0800

    代码优化

--- emucoo-manager/src/main/java/com/emucoo/manager/controller/sys/SysDeptController.java
+++ emucoo-manager/src/main/java/com/emucoo/manager/controller/sys/SysDeptController.java
@@ -151,11 +151,11 @@ public class SysDeptController extends BaseResource {
 		sysUserRelationService.saveSelective(sysUserRelation);
 		if(jedisCluster.exists(ISystem.IUSER.USER_RECENT + sysUserRelation.getParentUserId())){
 			String userIdStr=jedisCluster.get(ISystem.IUSER.USER_RECENT + sysUserRelation.getParentUserId());
-			if(StringUtil.isNotEmpty(userIdStr)){
-				String[] idArr = userIdStr.split(",");
+			String[] idArr = userIdStr.split(",");
+			if(null!=idArr && idArr.length>0) {
 				//判断是否存在该下级
-				List<String> userIdList= Arrays.asList(idArr);
-				if(!userIdList.contains(sysUserRelation.getUserId().toString())){
+				List<String> userIdList = Arrays.asList(idArr);
+				if (!userIdList.contains(sysUserRelation.getUserId().toString())) {
 					userIdList.add(sysUserRelation.getUserId().toString());
 					jedisCluster.set(ISystem.IUSER.USER_RECENT + sysUserRelation.getParentUserId(), StringUtils.join(userIdList, ","));
 				}
