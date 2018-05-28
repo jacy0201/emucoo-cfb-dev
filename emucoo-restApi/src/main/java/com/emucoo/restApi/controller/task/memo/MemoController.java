e4a5f93c6eb9e1a92699262b9792535b453fa6e5
jacy <jacy@emucoo.com>
2018-05-28 09:41:07 +0800

    代码优化

--- emucoo-restApi/src/main/java/com/emucoo/restApi/controller/task/memo/MemoController.java
+++ emucoo-restApi/src/main/java/com/emucoo/restApi/controller/task/memo/MemoController.java
@@ -10,10 +10,13 @@ import com.emucoo.restApi.sdk.token.ReSubmitTokenManager;
 import com.emucoo.restApi.sdk.token.UserTokenManager;
 import com.emucoo.service.sys.UserService;
 import com.emucoo.service.task.LoopWorkService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Api(description = "工作备忘接口")
 @RequestMapping("/api/task/memo")
 public class MemoController extends AppBaseController {
 
@@ -26,6 +29,7 @@ public class MemoController extends AppBaseController {
 	/**
      *  取得防止重复提交的token
 	 */
+    @ApiOperation(value = "取得防止重复提交的token")
     @PostMapping("/fetchSubmitToken")
 	public AppResult<SubmitTokenVo_O> fetchSubmitToken() {
 		SubmitTokenVo_O submitToken = new SubmitTokenVo_O();
@@ -38,6 +42,7 @@ public class MemoController extends AppBaseController {
      * @param params
      * @return
      */
+    @ApiOperation(value = "取联系人列表")
     @PostMapping("/fetchContacts")
     public AppResult<ContactsVo_O> fetchContacts(@RequestBody ParamVo<ContactsVo_I> params) {
 	    ContactsVo_I contactsVo_i = params.getData();
@@ -51,6 +56,7 @@ public class MemoController extends AppBaseController {
      * @param params
      * @return
      */
+    @ApiOperation(value = "编辑工作备忘")
     @PostMapping("/editMemo")
 	public AppResult editMemo(@RequestBody ParamVo<MemoEditVo_I> params) {
         SysUser user = UserTokenManager.getInstance().currUser(request.getHeader("userToken"));
@@ -70,6 +76,7 @@ public class MemoController extends AppBaseController {
      * @param params
      * @return
      */
+    @ApiOperation(value = "删除备忘")
     @PostMapping("/deleteMemo")
     public AppResult deleteMemo(@RequestBody ParamVo<MemoDeleteVo_I> params) {
         SysUser user = UserTokenManager.getInstance().currUser(request.getHeader("userToken"));
@@ -86,6 +93,7 @@ public class MemoController extends AppBaseController {
      * @param params
      * @return
      */
+    @ApiOperation(value = "创建工作备忘")
     @PostMapping("/createMemo")
     public AppResult createMemo(@RequestBody ParamVo<MemoCreationVo_I> params) {
         SysUser user = UserTokenManager.getInstance().currUser(request.getHeader("userToken"));
@@ -103,10 +111,10 @@ public class MemoController extends AppBaseController {
 
     /**
      * 查看工作备忘详细信息
-     * @param userToken
      * @param params
      * @return
      */
+    @ApiOperation(value = "查看工作备忘详细信息")
     @PostMapping("/memoDetail")
 	public AppResult<MemoDetailVo_O> memoDetail(@RequestBody ParamVo<MemoDetailVo_I> params) {
         SysUser loginUser = UserTokenManager.getInstance().currUser(request.getHeader("userToken"));
