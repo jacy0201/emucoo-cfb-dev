8441603e84f3896fbd68aeb6a6740020efeedad1
Shayne <shayne@emucoo.com>
2018-05-28 10:41:52 +0800

    job handler name can not be chinese

--- emucoo-job/src/main/java/com/emucoo/job/handle/CommonTaskBuildHandler.java
+++ emucoo-job/src/main/java/com/emucoo/job/handle/CommonTaskBuildHandler.java
@@ -7,7 +7,7 @@ import com.xxl.job.core.handler.annotation.JobHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@JobHandler(value = "常规任务根据任务定义创建每天执行的任务实例")
+@JobHandler(value = "CommonTaskInstanceCreator")
 @Component
 public class CommonTaskBuildHandler extends IJobHandler {
 
