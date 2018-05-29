e2e11626225b8a2ecdd57d25d4fdee41c3e6a3b6
Shayne <shayne@emucoo.com>
2018-05-29 18:44:45 +0800

    add unit test

---
+++ emucoo-manager/src/test/java/com/emucoo/manager/TestTaskManagerService.java
@@ -0,0 +1,29 @@
+package com.emucoo.manager;
+
+import com.emucoo.service.task.TaskCommonService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestTaskManagerService {
+
+    @Autowired
+    private TaskCommonService taskCommonService;
+
+    @Test
+    public void testSwitchCommonTask() {
+        List<Long> ids = new ArrayList<>();
+        ids.add(84L);
+
+        taskCommonService.switchCommonTask(ids, true);
+        Assert.assertTrue(true);
+    }
+}
