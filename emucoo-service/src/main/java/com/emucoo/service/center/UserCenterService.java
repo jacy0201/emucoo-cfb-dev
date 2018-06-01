1fdfb4684aa11e7a06f5916fd10b964ee15ad8ac
jacy <jacy@emucoo.com>
2018-06-01 14:31:11 +0800

    代码优化

--- emucoo-service/src/main/java/com/emucoo/service/center/UserCenterService.java
+++ emucoo-service/src/main/java/com/emucoo/service/center/UserCenterService.java
@@ -10,19 +10,19 @@ import java.util.List;
 public interface UserCenterService extends BaseService<TLoopWork> {
 
     //我执行的任务
-    List<TaskVO> executeTaskList(Long userId);
+    List<TaskVO> executeTaskList(String month,Long userId);
 
     //我审核的任务
-    List<TaskVO> auditTaskList(Long userId);
+    List<TaskVO> auditTaskList(String month,Long userId);
 
     //我创建的任务
-    List<TaskVO> createTaskList(Long userId);
+    List<TaskVO> createTaskList(String month,Long userId);
 
     //我接收的报告
-    List<TaskVO> getReportList(Long userId);
+    List<TaskVO> getReportList(String month,Long userId);
 
     //巡店任务
-    List<TFrontPlan> frontPlanList(Long userId);
+    List<TFrontPlan> frontPlanList(String month,Long userId);
 
     //维修任务
 
