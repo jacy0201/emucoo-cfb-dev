3967db40975ca6ea4e530bf55277eed584edcb7d
liuhp <Emucoo2018>
2018-06-04 17:31:46 +0800

    修改jmx依赖

--- emucoo-service/src/main/java/com/emucoo/service/planStatistics/PlanStatisticsService.java
+++ emucoo-service/src/main/java/com/emucoo/service/planStatistics/PlanStatisticsService.java
@@ -1,13 +1,8 @@
 package com.emucoo.service.planStatistics;
 
-import com.emucoo.dto.base.ParamVo;
 import com.emucoo.dto.modules.statistics.plan.GeneratePlanReportIn;
-import com.emucoo.dto.modules.task.TaskParameterVo;
 import com.emucoo.model.RVRReportStatistics;
 import com.emucoo.model.TFormType;
-import jxl.write.WritableWorkbook;
-
-import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 /**
