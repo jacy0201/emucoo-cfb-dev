06a1a7b4447992562d8461b30b488a02c7bc2c7f
jacy <jacy@emucoo.com>
2018-06-06 11:48:58 +0800

    个人中心相关

--- emucoo-dto/src/main/java/com/emucoo/dto/modules/calendar/CalendarDelVO.java
+++ emucoo-dto/src/main/java/com/emucoo/dto/modules/calendar/CalendarDelVO.java
@@ -11,14 +11,8 @@ import lombok.Data;
 @Data
 @ApiModel(value="删除行事历参数")
 public class CalendarDelVO {
-    //任务 id
-    @ApiModelProperty(value="巡店任务id",name="id",required = false, notes="删除巡店任务时传此参数")
-    private String  id;
     @ApiModelProperty(value="workID",name="workID",required = true)
     private String workID;
-    @ApiModelProperty(value="subWorkID",name="subWorkID",required = true)
-    private String subWorkID;
-
     //任务类型：1-常规任务 2-指派任务 3-改善任务  4-巡店安排；5-工作备忘
     @ApiModelProperty(value="任务类型",name="workType",required = true ,notes = "1-常规任务 2-指派任务 3-改善任务  4-巡店安排；5-工作备忘")
     private Integer workType;
