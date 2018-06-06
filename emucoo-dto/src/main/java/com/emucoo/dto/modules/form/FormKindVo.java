a54c2e2e958223b7bbe782fba6adfce8ea320794
liuhp <Emucoo2018>
2018-06-06 18:57:12 +0800

    接口增加字段注释

--- emucoo-dto/src/main/java/com/emucoo/dto/modules/form/FormKindVo.java
+++ emucoo-dto/src/main/java/com/emucoo/dto/modules/form/FormKindVo.java
@@ -1,5 +1,7 @@
 package com.emucoo.dto.modules.form;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.List;
@@ -8,15 +10,25 @@ import java.util.List;
  * Created by sj on 2018/4/24.
  */
 @Data
+@ApiModel
 public class FormKindVo {
+    @ApiModelProperty(value = "是否完成", name = "isDone", required = true, example = "true")
     private Boolean isDone;
+    @ApiModelProperty(value = "类别id", name = "kindID", required = true, example = "1")
     private Long kindID;
+    @ApiModelProperty(value = "类别名称", name = "kindName", required = true, example = "A.出品")
     private String kindName;
+    @ApiModelProperty(value = "题项数组", name = "problemArray", required = true)
     private List<FormProblemVo> problemArray;
+    @ApiModelProperty(value = "总题目数", name = "problemNum")
     private Integer problemNum;
+    @ApiModelProperty(value = "得分率", name = "scoreRate")
     private Float scoreRate;
+    @ApiModelProperty(value = "错题数", name = "wrongNum")
     private Integer wrongNum;
+    @ApiModelProperty(value = "实际得分", name = "realScore")
     private Integer realScore;
+    @ApiModelProperty(value = "实际总分，扣除NA", name = "realTotal")
     private Integer realTotal;
 
 }
--- emucoo-dto/src/main/java/com/emucoo/dto/modules/form/FormSubProblemVo.java
+++ emucoo-dto/src/main/java/com/emucoo/dto/modules/form/FormSubProblemVo.java
@@ -1,5 +1,6 @@
 package com.emucoo.dto.modules.form;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.List;
@@ -9,9 +10,14 @@ import java.util.List;
  */
 @Data
 public class FormSubProblemVo {
+    @ApiModelProperty(value = "机会点数组", name = "subProblemChanceArray")
     private List<FormChanceVo> subProblemChanceArray;
+    @ApiModelProperty(value = "子题id", name = "subProblemID", required = true, example = "1")
     private Long subProblemID;
+    @ApiModelProperty(value = "子题名称", name = "subProblemName", required = true, example = "a.披萨1")
     private String subProblemName;
+    @ApiModelProperty(value = "子题得分", name = "subProblemScore", example = "1")
     private Integer subProblemScore;
+    @ApiModelProperty(value = "子题总分", name = "subProblemTotal", example = "5")
     private Integer subProblemTotal;
 }
