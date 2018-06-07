e06e74f31c121e4abb23627eeccfde21d6c1101f
liuhp <Emucoo2018>
2018-06-07 16:23:02 +0800

    修改bug

--- emucoo-service/src/main/java/com/emucoo/service/manage/impl/FormManageServiceImpl.java
+++ emucoo-service/src/main/java/com/emucoo/service/manage/impl/FormManageServiceImpl.java
@@ -89,6 +89,9 @@ public class FormManageServiceImpl implements FormManageService {
     public void createForm(TFormMain form, Long userId) {
         form.setCreateTime(DateUtil.currentDate());
         form.setModifyTime(DateUtil.currentDate());
+        if(form.getFormType() == null) {
+            form.setFormType(FormType.RVR_TYPE.getCode());
+        }
         form.setCreateUserId(userId);
         form.setModifyUserId(userId);
         form.setIsDel(false);
