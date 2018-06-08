c47f56a2ca23f8e91615a3f2d7878c30bebb4e7a
jacy <jacy@emucoo.com>
2018-06-08 15:42:07 +0800

    t_task 添加字段 start_time 、end_time

--- db/modify.sql
+++ db/modify.sql
@@ -1,3 +1,3 @@
--- t_taks  添加 start_time 、end_time字段
-alter table t_taks add start_time varchar(255) default NULL COMMENT '开始时间： HH:mm';
-alter table t_taks add end_time varchar(255) default NULL COMMENT '结束时间： HH:mm';
+-- t_task 添加字段 start_time 、end_time
+alter table t_task add start_time varchar(50) default NULL COMMENT '开始时间： HH:mm';
+alter table t_task add end_time varchar(50) default NULL COMMENT '结束时间： HH:mm';
