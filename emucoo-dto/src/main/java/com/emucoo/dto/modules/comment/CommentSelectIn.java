8c46e0407c25e16a525bd66804653da6b72e5214
jacy <jacy@emucoo.com>
2018-06-13 14:32:20 +0800

    代码优化

--- emucoo-dto/src/main/java/com/emucoo/dto/modules/comment/CommentSelectIn.java
+++ emucoo-dto/src/main/java/com/emucoo/dto/modules/comment/CommentSelectIn.java
@@ -8,10 +8,11 @@ import lombok.Data;
 @ApiModel(value="查询评论/回复")
 public class CommentSelectIn {
 	//1：常规任务，2：指派任务：3：改善任务，4：巡店任务，5：工作备忘，6：评论
-	@ApiModelProperty(value="评论类型",name="workType",required = true,notes = "1：常规任务，2：指派任务：3：改善任务，4：巡店任务，5：工作备忘，6：评论")
+	@ApiModelProperty(value="评论类型",name="workType",required = true,notes = "1：常规任务，2：指派任务：3：改善任务，4：巡店任务，5：工作备忘，6：评论，7：维修任务")
 	private Integer workType;
 	private String workID;
 	private String subID;
+	private Long repairID;
 	@ApiModelProperty(value="报告Id",name="reportID",notes = "workType=4 巡店任务评论传此参数")
 	//如果 workType=4 需传reportID
 	private Long reportID;
