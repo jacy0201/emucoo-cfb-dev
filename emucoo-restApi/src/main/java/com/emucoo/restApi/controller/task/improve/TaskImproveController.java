4196bc5fe3810f3ca1350bc5cdbb90917faacfa2
Shayne <shayne@emucoo.com>
2018-06-13 17:29:22 +0800

    message builder

--- emucoo-restApi/src/main/java/com/emucoo/restApi/controller/task/improve/TaskImproveController.java
+++ emucoo-restApi/src/main/java/com/emucoo/restApi/controller/task/improve/TaskImproveController.java
@@ -16,49 +16,53 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+/**
+ * @author Shayne.Wang
+ * @date 2018-06-12
+ */
 @RestController
 @RequestMapping("/api/task/improve")
 public class TaskImproveController extends AppBaseController {
 
-	@Autowired
-	private TaskImproveService taskImproveService;
+    @Autowired
+    private TaskImproveService taskImproveService;
 
-	@PostMapping("/save")
-	public AppResult<String> save(@RequestBody ParamVo<TaskImproveVo> base) {
-		TaskImproveVo vo = base.getData();
-		SysUser user = UserTokenManager.getInstance().currUser(request.getHeader("userToken"));
-		taskImproveService.createImproveTask(vo, user);
-		return success("");
-	}
+    @PostMapping("/save")
+    public AppResult<String> save(@RequestBody ParamVo<TaskImproveVo> base) {
+        TaskImproveVo vo = base.getData();
+        SysUser user = UserTokenManager.getInstance().currUser(request.getHeader("userToken"));
+        taskImproveService.createImproveTask(vo, user);
+        return success("");
+    }
 
-	@PostMapping("/submit")
-	public AppResult<String> submit(@RequestBody ParamVo<TaskImproveSubmitIn> base) {
-		TaskImproveSubmitIn vo = base.getData();
-		SysUser user = UserTokenManager.getInstance().currUser(request.getHeader("userToken"));
-		taskImproveService.submitImproveTask(vo, user);
-		return success("");
-	}
+    @PostMapping("/submit")
+    public AppResult<String> submit(@RequestBody ParamVo<TaskImproveSubmitIn> base) {
+        TaskImproveSubmitIn vo = base.getData();
+        SysUser user = UserTokenManager.getInstance().currUser(request.getHeader("userToken"));
+        taskImproveService.submitImproveTask(vo, user);
+        return success("");
+    }
 
-	@PostMapping("/audit")
-	public AppResult<String> audit(@RequestBody ParamVo<TaskImproveAuditIn> base) {
-		TaskImproveAuditIn vo = base.getData();
-		SysUser user = UserTokenManager.getInstance().currUser(request.getHeader("userToken"));
-		taskImproveService.auditImproveTask(vo, user);
-		return success("");
-	}
+    @PostMapping("/audit")
+    public AppResult<String> audit(@RequestBody ParamVo<TaskImproveAuditIn> base) {
+        TaskImproveAuditIn vo = base.getData();
+        SysUser user = UserTokenManager.getInstance().currUser(request.getHeader("userToken"));
+        taskImproveService.auditImproveTask(vo, user);
+        return success("");
+    }
 
-	@PostMapping("/detail")
-	public AppResult<TaskImproveDetailOut> detail(@RequestBody ParamVo<TaskImproveDetailIn> base) {
-		TaskImproveDetailIn vo = base.getData();
-		TaskImproveDetailOut out = taskImproveService.viewImproveTaskDetail(vo);
-		return success(out);
-	}
+    @PostMapping("/detail")
+    public AppResult<TaskImproveDetailOut> detail(@RequestBody ParamVo<TaskImproveDetailIn> base) {
+        TaskImproveDetailIn vo = base.getData();
+        TaskImproveDetailOut out = taskImproveService.viewImproveTaskDetail(vo);
+        return success(out);
+    }
 
-	@PostMapping("/getChancePointInfo")
-	public TaskChancePointInfo getChancePointInfo(@RequestBody Map map) {
-		System.out.println(new Gson().toJson(map));
-		return new TaskChancePointInfo();
+    @PostMapping("/getChancePointInfo")
+    public TaskChancePointInfo getChancePointInfo(@RequestBody Map map) {
+        System.out.println(new Gson().toJson(map));
+        return new TaskChancePointInfo();
 
-	}
+    }
 
 }
--- emucoo-service/src/main/java/com/emucoo/service/task/TaskImproveService.java
+++ emucoo-service/src/main/java/com/emucoo/service/task/TaskImproveService.java
@@ -5,19 +5,43 @@ import com.emucoo.model.SysUser;
 
 /**
  * 改善任务
- * 
+ *
  * @author zhangxq
  * @date 2018-03-20
  */
 public interface TaskImproveService {
 
-	void createImproveTask(TaskImproveVo vo, SysUser user);
+    /**
+     * 创建改善任务
+     *
+     * @param vo
+     * @param user
+     */
+    void createImproveTask(TaskImproveVo vo, SysUser user);
 
-	void submitImproveTask(TaskImproveSubmitIn taskImproveSubmitIn, SysUser user);
+    /**
+     * 执行改善任务
+     * @param taskImproveSubmitIn
+     * @param user
+     */
+    void submitImproveTask(TaskImproveSubmitIn taskImproveSubmitIn, SysUser user);
 
-	void auditImproveTask(TaskImproveAuditIn taskImproveAuditIn, SysUser user);
+    /**
+     * 审核改善任务
+     * @param taskImproveAuditIn
+     * @param user
+     */
+    void auditImproveTask(TaskImproveAuditIn taskImproveAuditIn, SysUser user);
 
-	TaskImproveDetailOut viewImproveTaskDetail(TaskImproveDetailIn taskImproveDetailIn);
+    /**
+     * 改善任务详情
+     * @param taskImproveDetailIn
+     * @return
+     */
+    TaskImproveDetailOut viewImproveTaskDetail(TaskImproveDetailIn taskImproveDetailIn);
 
+    /**
+     * 定时任务创建改善任务实例
+     */
     void buildImproveTaskInstance();
 }
